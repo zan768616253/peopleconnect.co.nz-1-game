2e189563a6ffa08b8d637c884f782eac31ba4748
ericw <eric.wang@dbvisit.com>
2023-01-29 12:07:22 +1300

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+import { Background } from './background.js';
+import {FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js';
+
+
+window.addEventListener("load", function(){
+
+	const canvas = document.getElementById('canvas1');
+	const ctx = canvas.getContext('2d');
+	canvas.width = 500;
+	canvas.height = 500;
+
+	class Game {
+		constructor(width, height){
+			this.width = width;
+			this.height = height;
+			this.groundMargin = 80;
+			this.speed = 0;
+			this.maxSpeed =3;
+			this.background = new Background(this);
+			this.player = new Player(this);
+			this.input = new InputHandler();
+			this.enemies = [];
+			this.enemyTimer = 0;
+			this.enemyInterval = 1000;
+		}
+		update(deltaTime){
+			this.background.update();
+			this.player.update(this.input.keys, deltaTime);
+			//handleEnemies
+			if (this.enemyTimer > this.enemyInterval){
+				this.addEnemy();
+				this.enemyTimer = 0;
+			} else {
+				this.enemyTimer += deltaTime;
+			}
+			this.enemies.forEach(enemy => {
+				enemy.update(deltaTime);
+				if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+			})
+
+		}
+		draw(context){
+			this.background.draw(context);
+			this.player.draw(context);	
+			this.enemies.forEach(enemy => {
+				enemy.draw(context);
+			});
+		}
+		addEnemy(context){
+			if(this.speed > 0 && Math.random()< 0.5) this.enemies.push(new GroundEnemy(this));
+			else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+			this.enemies.push(new FlyingEnemy(this));
+			console.log(this.enemies);
+		}
+	}
+	const game = new Game(canvas.width, canvas.height);
+	console.log(game);
+	let lastTime = 0;
+
+
+	
+	function animate(timeStamp){
+		const deltaTime = timeStamp - lastTime;
+		lastTime = timeStamp;
+		ctx.clearRect(0,0,canvas.width, canvas.height);
+		game.update(deltaTime);
+		game.draw(ctx);
+		requestAnimationFrame(animate);
+	}
+	animate(0);
+});
---
+++ playerStates.js
@@ -0,0 +1,116 @@
+const states = {
+    SITTING: 0,
+    RUNNING: 1,
+    JUMPING: 2,
+    FALLING: 3,
+    //ROLLING: 4,
+}
+
+class State {
+    constructor(state){
+        this.state = state;
+    }
+}
+
+export class Sitting extends State{
+    constructor(player){
+        super('SITTING');
+        this.player = player;
+    }
+    enter(){
+        this.player.frameX = 0;
+        this.player.maxFrame = 4;
+        this.player.frameY = 5;
+    }
+    handleInput(input){
+        if(input.includes('ArrowLeft') || input.includes('ArrowRight')){
+            this.player.setState(states.RUNNING, 1);
+        } 
+    }
+}
+
+export class Running extends State{
+    constructor(player){
+        super('RUNNING');
+        this.player = player;
+    }
+    enter(){
+        this.player.frameX = 0;
+        this.player.maxFrame = 8;
+        this.player.frameY = 3;
+        this.player.maxSpeed =10;
+    }
+    handleInput(input){
+        if(input.includes('ArrowDown')){
+            this.player.setState(states.SITTING, 0);
+        } else if (input.includes ('ArrowUp')){
+            this.player.setState(states.JUMPING, 1);
+        }
+       /* else if (input.includes ('Enter')){
+            this.player.setState(states.ROLLING);
+        }*/
+    }
+}
+
+export class Jumping extends State{
+    constructor(player){
+        super('JUMPING');
+        this.player = player;
+    }
+    enter(){
+       if(this.player.onGround()) this.player.vy -=20;
+        this.player.frameX = 0;
+        this.player.maxFrame = 6;
+        this.player.frameY = 1;
+        this.player.maxSpeed =10;
+    }
+    handleInput(input){
+        if(this.player.vy > this.player.weight){
+            this.player.setState(states.FALLING, 1);
+        }
+    }
+}
+
+export class Falling extends State{
+    constructor(player){
+        super('FALLING');
+        this.player = player;
+    }
+    enter(){
+        this.player.frameX = 0;
+        this.player.maxFrame = 6;
+        this.player.frameY = 2;
+        this.player.maxSpeed =10; 
+    }
+    handleInput(input){
+        if(this.player.onGround()){
+            this.player.setState(states.RUNNING, 1);
+        }
+    }
+}
+
+/*export class Rolling extends State{
+    constructor(player){
+        super('ROLLING');
+        this.player = player;
+    }
+    enter(){
+        this.player.frameX = 0;
+        this.player.maxFrame = 6;
+        this.player.frameY = 6;
+        this.player.maxSpeed =25; 
+    }
+    handleInput(input){
+        if(input.includes('ArrowDown')){
+            if(this.player.onGround())  this.player.setState(states.SITTING);
+        } else if (input.includes ('ArrowUp')){
+            if(this.player.onGround()) this.player.vy -=25;
+        }
+        else if(input.includes('ArrowDown')){
+            if(!this.player.onGround())  this.player.weight = 50;
+        }
+        else if (input.includes ('Enter')){
+            this.player.setState(states.RUNNING);
+        }
+    }
+}*/
